598ad8f99a7a7bd464e221dd6aa3f64b59839027
Tomas <tomasloon@gmail.com>
2020-03-30 14:53:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const black = '#000000';
+const red = '#E52222';
+const green = '#A6E32D';
+const yellow = '#FC951E';
+const blue = '#C48DFF';
+const magenta = '#FA2573';
+const cyan = '#67D9F0';
+const white = '#F2F2F2';
+
 exports.decorateConfig = (config) => {
   const defaultCSS = `
   .header_appTitle {
@@ -48,27 +57,27 @@ exports.decorateConfig = (config) => {
   }
   `;
   return Object.assign({}, config, {
-    fontFamily: 'Fira Code',
+    fontFamily: 'Roboto Mono',
     fontSize: 18,
     cursorColor: 'rgba(255,255,255,0.5)',
     cursorAccentColor: '#fff',
     colors: {
-      black: '#000000',
-      red: '#E52222',
-      green: '#A6E32D',
-      yellow: '#FC951E',
-      blue: '#C48DFF',
-      magenta: '#FA2573',
-      cyan: '#67D9F0',
-      white: '#F2F2F2',
-      lightBlack: '#666666',
-      lightRed: '#E50000',
-      lightGreen: '#00D900',
-      lightYellow: '#E5E500',
-      lightBlue: '#0000FF',
-      lightMagenta: '#E500E5',
-      lightCyan: '#00E5E5',
-      lightWhite: '#E5E5E5',
+      black,
+      red,
+      green,
+      yellow,
+      blue,
+      magenta,
+      cyan,
+      white,
+      lightBlack: LightenDarkenColor(black, 50),
+      lightRed: LightenDarkenColor(red, 50),
+      lightGreen: LightenDarkenColor(green, 50),
+      lightYellow: LightenDarkenColor(yellow, 50),
+      lightBlue: LightenDarkenColor(blue, 50),
+      lightMagenta: LightenDarkenColor(magenta, 50),
+      lightCyan: LightenDarkenColor(cyan, 50),
+      lightWhite: LightenDarkenColor(white, 50),
     },
     css: `
     ${config.css || ''}
@@ -87,3 +96,25 @@ exports.getTabsProps = (parentProps, props) => {
   }
   return Object.assign({}, parentProps, props)
 }
+
+/** @see https://css-tricks.com/snippets/javascript/lighten-darken-color/ */
+function LightenDarkenColor(col, amt) {
+  var usePound = false;
+
+  if (col[0] === "#") {
+    col = col.slice(1);
+    usePound = true;
+  }
+
+  var num = parseInt(col,16);
+  var r = (num >> 16) + amt;
+  if (r > 255) r = 255;
+  else if  (r < 0) r = 0;
+  var b = ((num >> 8) & 0x00FF) + amt;
+  if (b > 255) b = 255;
+  else if  (b < 0) b = 0;
+  var g = (num & 0x0000FF) + amt;
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+  return (usePound ? '#' : '') + (g | (b << 8) | (r << 16)).toString(16);
+};
